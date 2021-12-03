db12cbe120cab53544565c2a04bdae3460485f1e
Ivainge <ivainge@gmail.com>
2021-12-03 11:59:56 +0300

    Add command

---
+++ commands/blya_doebalis.js
@@ -0,0 +1,6 @@
+module.exports = async (bot,message,args,argsF) => {
+
+    message.reply({ content: '<:blya_doebalis:893991241728004096>', allowedMentions: { repliedUser: false }});
+    
+};
+module.exports.names = ["сука", "блять", "ебаный", "хуита", "сука,", "блять,", "ебаный,", "хуита,"];
